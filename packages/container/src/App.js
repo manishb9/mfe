c9d61e40797cea19c61c0d318663cee951f07cca
manishb9 <manishb@hotmail.com>
2022-05-23 17:11:06 +0530

    added generate class name param

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -8,13 +8,14 @@ const generateClassName = createGenerateClassName({
 })
 export default () => {
   return (
-    <StylesProvider generateClassName={generateClassName}>
-      <BrowserRouter>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+     
         <div>
           <Header />
           <MarketingApp />
         </div>
-      </BrowserRouter> 
-    </StylesProvider>
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
